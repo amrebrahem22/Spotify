7a22afefd14fbe9dbf3ebdfac683bf429eb71fe6
amrebrahem22 <amrebrahem226@gmail.com>
2021-11-06 02:57:00 +0200

    create the context and reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
 import Login from './pages/Login/Login';
 import { getTokenFromResponse } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import { useStateValue } from "./StateProvider";
+
+const s = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState('');
 
   useEffect(() => {
-    let token = getTokenFromResponse();
+    let hash = getTokenFromResponse();
+
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+
+      s.setAccessToken(_token);
+
+      s.getMe().then(user => {
+        console.log(user);
+      })
+    }
+
     console.log('I got the token -> ', token);
-  }, [])
+
+  }, [token])
 
   return (
     <div className="app">
