385c12e10fa4e2a0db46d0485c49f6e03e2cc14c
amrebrahem22 <amrebrahem226@gmail.com>
2021-11-07 05:04:30 +0200

    finish footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import './footer.css'
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import Slider from '@mui/material/Slider';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
+import VolumeDownIcon from '@mui/icons-material/VolumeDown';
+import './footer.css';
+import albumImg from '../../images/album.jpg'
 
 const Footer = () => {
     return (
         <div className="footer">
-            Footer
+            <div className="footer__left">
+                <img src={albumImg} alt="Album" className="footer__albumLogo" />
+                <div className="footer__songInfo">
+                    <h4>Yeah!</h4>
+                    <p>Usher</p>
+                </div>
+            </div>
+            <div className="footer__center">
+                <ShuffleIcon className="footer__green" />
+                <SkipPreviousIcon className="footer__icon" />
+                <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+                <SkipNextIcon className="footer__icon" />
+                <RepeatIcon className="footer__green" />
+            </div>
+            <div className="footer__right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider aria-label="Volume"  />
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     )
 }
